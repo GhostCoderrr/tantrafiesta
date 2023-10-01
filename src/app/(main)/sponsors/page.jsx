27d24301235767786160ec34e33178c5db50a0e4
GhostCoderrr <surferonacidpos@gmail.com>
2023-10-01 12:23:47 +0530

    sponsors text visible now

--- src/app/(main)/sponsors/page.jsx
+++ src/app/(main)/sponsors/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const page = () => {
   return (
     <div className="h-screen w-screen grid bg-black grid-cols-1 md:grid-cols-2 items-center">
-      <div className="text-6xl font-bold w-full grid place-items-center pt-24 md:pt-0">
+      <div className="text-6xl font-bold text-white w-full grid place-items-center pt-24 md:pt-0">
         SPONSORS
       </div>
       <div className="h-full flex flex-col  md:pt-28 pb-5 justify-between items-center md:items-start">
